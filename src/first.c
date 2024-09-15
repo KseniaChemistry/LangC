8a9213bc4bdf17684b739fd2709d83e3e5bc45d1
Ksenia <denisova.moderator@gmail.com>
2024-09-15 10:36:59 +0300

    Int

--- src/first.c
+++ src/first.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+int main (void)
 
-int main (void) {
-    printf("Hello World!");
+{
+    int num;
+    num = 1;
+
+    printf("Я простой ");
+    printf("комьпютер.\n");
+    printf("Моей любимой цифрой является %d, так как она первая.\n", num);
     return 0;
 }
